b75ca398bc83a731f772ac94be6a56ef3be43f46
jooker302 <i.omer6637@gmail.com>
2023-08-18 03:14:27 -0700

    Three Sections Done

--- app/page.js
+++ app/page.js
@@ -1,113 +1,122 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { faTelegram, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div
+        id="socialbar"
+        class="flex justify-center bg-red-500 py-1 px-4 items-center"
+      >
+        <div className="flex justify-between">
+          <a className="m-2" href="https://t.me/pinkbird">
+            <FontAwesomeIcon
+              icon={faXTwitter}
+              size="lg"
+              style={{ color: "#ffffff", width: "24px", height: "24px" }}
+            />
+          </a>
+          <a className="m-2" href="https://t.me/pinkbird">
+            <FontAwesomeIcon
+              icon={faTelegram}
+              size="lg"
+              style={{ color: "#ffffff", width: "24px", height: "24px" }}
             />
           </a>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        id="mainbar"
+        class="flex justify-between bg-orange-500 py-1 px-4 items-center"
+      >
+        <div>
+          <img className="w-32 h-32" src="/main.jpeg" />
+        </div>
+        <div className="flex justify-center p-4">
+          <a className="px-2 md:text-2xl" href="">
+            Home
+          </a>
+          <a className="px-2 md:text-2xl" href="">
+            About
+          </a>
+          <a className="px-2 md:text-2xl" href="">
+            Team
+          </a>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <div id="intro" className="flex justify-between p-4 flex-wrap">
+        <div className="md:w-1/2">
+          <img className="md:w-96 md:h-96 md:ml-28" src="/main.jpeg" />
+        </div>
+        <div className="md:w-1/2 px-4 py-2">
+          <h2 className="text-5xl font-bold underline">BARDOGE</h2>
+          <p className="p-4 pt-8 text-xl">
+            Introducing BARDOGE, a memecoin that ingeniously merges the worlds
+            of Barbie and Doge, resulting in a captivating and contemporary
+            cryptocurrency phenomenon. The coin's logo is a delightful fusion of
+            Barbie's iconic outline and Doge's endearing Shiba Inu, embodying
+            the essence of both cultural symbols. BARDOGE sets out to combine
+            Barbie's sophistication and trendsetting allure with Doge's playful
+            and meme-centric charm.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="flex justify-center my-3">
+            <button className="px-4 py-2 bg-red-400 rounded-lg">
+              About Us
+            </button>
+          </div>
+        </div>
       </div>
-    </main>
-  )
+
+      <div class="flex justify-center py-8 flex-wrap">
+        <div class="w-full md:w-1/4 my-6 md:my-0 bg-white shadow-md rounded-lg md:mx-4">
+          <div class="relative">
+            <img
+              src="/main.jpeg"
+              alt="Image 2"
+              class="absolute top-0 w-12 h-12 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full"
+            />
+          </div>
+          <div class="p-4">
+            <h2 class="text-xl font-semibold mb-2 mt-4">Launching On</h2>
+            <p class="text-gray-600">Bardoge is listed on Arbitrum Chain</p>
+          </div>
+        </div>
+
+        <div class="w-full md:w-1/4 bg-white my-6 md:my-0 shadow-md rounded-lg md:mx-4">
+          <div class="relative">
+            <img
+              src="/main.jpeg"
+              alt="Image 2"
+              class="absolute top-0 w-12 h-12 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full"
+            />
+          </div>
+          <div class="p-4">
+            <h2 class="text-xl font-semibold mb-2 mt-4">
+              Estimated Launch Date
+            </h2>
+            <p class="text-gray-600">
+              Bardoge is going to launch on August 17, 2023
+            </p>
+          </div>
+        </div>
+
+        <div class="w-full md:w-1/4 bg-white my-6 md:my-0 shadow-md rounded-lg md:mx-4">
+          <div class="relative">
+            <img
+              src="/main.jpeg"
+              alt="Image 2"
+              class="absolute top-0 w-12 h-12 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full"
+            />
+          </div>
+          <div class="p-4">
+            <h2 class="text-xl font-semibold mb-2 mt-4">Token Symbol</h2>
+            <p class="text-gray-600">Bardoge token symbol is $BDOGE</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
